a4c8800871591d1d135d81f29575aeb5416ff97a
Luis P <luis.prieto@epfl.ch>
2014-06-27 17:54:23 +0200

    Added data about the strategy question coding, and integrated into the preprocessing

--- JDCSurveyPreprocess.R
+++ JDCSurveyPreprocess.R
@@ -21,6 +21,22 @@ preprocessJDCSurveys <- function(rootDir){
   # Give some meaningful name to the sequence factor
   data$Sequence <- factor(data$Sequence,labels=c("Discrete.First","Continuous.First"))
   
+  # We import the data from the coding of the strategy question
+  data2 <- read.csv("JdC-StrategyQuestion.csv")
+  # We correct group labelling due to lamp changes in session 2
+  # First, we convert the group variable to character
+  data2$Group.Number <- as.character(data2$Code)
+  data2[data2$Group.Number=="S2G4","Group.Number"] <- "S2G3"
+  data2[data2$Group.Number=="S2G5","Group.Number"] <- "S2G4"
+  data2$Group.Number <- as.factor(data2$Group.Number)
+  
+  data2$Mentions.Fractions <- data2$Does.the.group.talk.about.fractions..FINAL. == 1
+  data2$Gets.Mechanic <- data2$Does.the.group.understand.the.rule.of.the.game..FINAL_Temp. == 1  
+  
+  data2 <- data2[,c("Group.Number","Mentions.Fractions","Gets.Mechanic")]
+  
+  data <- merge(data,data2,by="Group.Number")
+  
   # save the R data frame
   save(data,file="Survey.rda",compress=TRUE)
   
