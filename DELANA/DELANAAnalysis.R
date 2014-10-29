96cce14524e5321d079eb0cf7d1d2163925fd987
Luis P <lprisan@hotmail.com>
2014-10-29 18:09:42 +0100

    Added way of extracting video snippets to ask participants

--- DELANA/DELANAAnalysis.R
+++ DELANA/DELANAAnalysis.R
@@ -349,3 +349,58 @@ doVideoCodingAnalysis <- function(){
     
     totaldata
 }
+
+
+generateExtractVideoSnippets <- function(videoDir = ".",window=10){
+    
+    snippetData <- read.csv("TimesToExtractVideo.csv",sep=";")
+
+    sessions <- unique(snippetData$Session)
+    
+    originalDir <- getwd()
+    setwd(videoDir)
+    
+    fileConn<-file("extractSnippets.sh")
+
+    lines <- "#!/bin/bash"
+    
+    for (i in 1:nrow(snippetData)){
+        Window.Center <- snippetData[i,"Window.Center"]
+        
+        startTime <- Window.Center - (window/2)*1000
+        endTime <- Window.Center + (window/2)*1000
+        
+        originalFile <- paste(snippetData$Session[[i]],"-video.avi",sep="")
+        #print(paste("Processing video... ",originalFile))
+        #if(!file.exists(originalFile)){
+        #    print("Missing video!")
+        #}
+        
+        snippetLabel <- paste(snippetData$Session[[i]],"-snippet",i,sep="")
+        
+        command <- paste("mencoder -ss ",msToHMS(startTime)," -endpos 00:00:10 -oac copy -ovc copy ",originalFile," -o ",snippetLabel,".avi\n", sep="")
+        lines[i+1] <- command
+        #command2 <- paste("ffmpeg -ss ",msToHMS(startTime)," -t 00:00:10 -i ",originalFile," -vcodec copy -acodec copy ",snippetLabel,".avi", sep="")
+        #system(command)
+    }
+    
+    writeLines(lines,fileConn)
+    
+    close(fileConn)
+    
+    setwd(originalDir)
+    #print(paste("Check for your extract command in",videoDir))
+}
+
+msToHMS <- function(ms){
+    H <- floor(ms/(60*60*1000))
+    M <- floor((ms-(H*60*60*1000))/(60*1000))
+    S <- floor((ms-(H*60*60*1000)-(M*60*1000))/1000)
+    
+    
+    H <- formatC(H, width = 2, format = "d", flag = "0")
+    M <- formatC(M, width = 2, format = "d", flag = "0")
+    S <- formatC(S, width = 2, format = "d", flag = "0")
+    
+    paste(H,M,S,sep=":")
+}
